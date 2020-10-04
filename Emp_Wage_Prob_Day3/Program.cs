0eb84a49e1670afd99c49c23ab782002f6d28538
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-04 06:31:44 +0530

    UC1: Attendance algo of the employee

--- Emp_Wage_Prob_Day3/Program.cs
+++ Emp_Wage_Prob_Day3/Program.cs
@@ -6,7 +6,16 @@ namespace Emp_Wage_Prob_Day3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Employee Wage Computation Program");
+            Random randomNum = new Random();
+            int attendance = randomNum.Next(0, 3);
+            if (attendance == 0)
+                Console.WriteLine("Employee is absent");
+            if (attendance == 1)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is part time");
+
         }
     }
 }
