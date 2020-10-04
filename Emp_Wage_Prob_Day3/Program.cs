3e69cf495971a759b798016db0489e397e0e2dea
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-04 06:07:24 +0530

    Initial Commit

---
+++ Emp_Wage_Prob_Day3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_Prob_Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
